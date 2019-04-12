9edd71d393f84a41692e88472098e011311a7764
Mustafa Ghasemi <mostafa_ghasemi63@yahoo.com>
2019-04-12 11:01:23 -0700

    add the prototype for each pizza price by size

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
 //bussines logic
+
+//Pizza constructor
 function Pizza(size,toppings) {
   this.size = size,
-  this.toppings
+  this.toppings = toppings,
+  this.price = 8;
+  this.priceBySize();
+  this.PriceByToppings();
 }
-
+Pizza.prototype.priceBySize = function() {
+  if(this.size === "large"){
+    this.price += 2
+  }else if (this.size === "medium"){
+    this.price;
+  }else if(this.pizza === "small"){
+    this.price -= 2
+  }
+}
 
 
 
@@ -13,7 +26,7 @@ $(document).ready(function(){
     event.preventDefault();
     var size = $("#size").val();
     var toppings = $("input:checkbox[name=topping]:checked").length;
-
+    var newPizza = new Pizza(size,toppings);
     console.log(size);
     console.log(toppings);
 
